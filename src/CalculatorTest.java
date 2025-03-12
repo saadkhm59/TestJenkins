7076ff660cb2933052cb620785eefff415e375fd
saadkhm59 <152981499+saadkhm59@users.noreply.github.com>
2025-03-12 11:45:16 +0100

    test1

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -1,7 +1,7 @@
-
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
+
+
+import org.testng.annotations.Test;
 
 public class CalculatorTest {
 
